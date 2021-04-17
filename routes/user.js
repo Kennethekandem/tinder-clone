6d978ebf9a38e72408bb2d20d5e071ab947aa347
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-17 22:15:16 +0100

    update

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const user = require('./user');
-
-router.use('/user', user);
 
 router.get('/', (req, res) => {
-    res.send("Hello World from Tinder");
+    res.send("Hello World from users");
 });
-
-
 
 module.exports = router;
