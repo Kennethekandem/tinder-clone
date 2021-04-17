e7b9a3867dd43de43cc168b3c4d9a6df1d9bce06
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-17 10:23:40 +0100

    update to routes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 require('dotenv/config');
+const bodyParser = require('body-parser');
+const routes = require('./routes/app');
+
+app.use('/', routes);
+app.use(bodyParser.json());
 
 
 const port = process.env.PORT || 5000;
