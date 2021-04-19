f76b2840e446982dc1df53b28f2cacd5d0ead3bd
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-19 11:42:49 +0100

    update to auth validator

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -4,12 +4,12 @@ const Schema = {
     register: joi.object().keys({
         name: joi.string().required(),
         email: joi.string().email().lowercase().required(),
-        phone: joi.number().min(10).max(11).required()
+        phone: joi.number().min(10).max(11).required(),
         password: joi.string().min(6).required(),
 
         // Other requirements
         age: joi.number().max(2).required(),
         gender: joi.string().max(1).required(),
         location: joi.string().required()
-    });
+    })
 }
