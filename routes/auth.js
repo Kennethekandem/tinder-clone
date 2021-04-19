d1a320f6407fed1a15a6c4f5d49b4a126618a42b
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-19 11:54:08 +0100

    update to schema and add validator middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,4 @@
 const router = require('express').Router;
-const validator = require('../validators/auth.validator');
-
+const schema = require('../schemas/auth.schema');
 
 module.exports = router;
